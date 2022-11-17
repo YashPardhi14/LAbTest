0d775705f03e5efadf3e8ea3824f2b26d114f457
YashPardhi14 <114048042+YashPardhi14@users.noreply.github.com>
2022-11-17 12:09:45 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,13 @@
+package Assignment3;
+
+public class Q1 {
+	//this block will executed first!
+static {
+	System.out.println("Calculating the sum");
+}
+	public static void main(String[] args) {
+		  System.out.println("sum of 3 + 7 is:"+ (3+7));
+
+	}
+
+}
